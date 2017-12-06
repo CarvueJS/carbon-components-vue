efce7b257fb2cd12b079dd312965852c802f0b48
Nicholas Lee <nicholaslee119@gmail.com>
2017-12-06 22:46:09 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div data-loading=""></div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { Loading } from 'carbon-components';
+
+export default {
+  name: 'app',
+  mounted() {
+    Loading.create(window.document.querySelector('[data-loading]'));
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

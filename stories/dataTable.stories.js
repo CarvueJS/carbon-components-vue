19d92413d94006d3fcff35b45551a6cb44ac1fed
nicholaslee119 <nicholaslee119@gmail.com>
2018-02-02 10:14:29 +0900

    add story of dataTable

---
+++ stories/dataTable.stories.js
@@ -0,0 +1,38 @@
+import { storiesOf } from '@storybook/vue';
+
+storiesOf('DataTable', module)
+  .addWithJSX('normal', () => ({
+    template: `
+    <ca-data-table>
+      <thead>
+
+        <tr>
+          <th>
+            head1
+          </th>
+          <th>
+            head2
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>
+            itemA
+          </th>
+          <th>
+            itemB
+          </th>
+        </tr>
+        <tr>
+          <th>
+            itemE
+          </th>
+          <th>
+            itemD
+          </th>
+        </tr>
+      </tbody>
+    </ca-data-table>
+    `,
+  }));

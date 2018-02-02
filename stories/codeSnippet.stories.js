850b06cc04647162bc8828dd47f364f593429349
nicholaslee119 <nicholaslee119@gmail.com>
2018-02-02 10:11:19 +0900

    improve tooltip

---
+++ stories/codeSnippet.stories.js
@@ -0,0 +1,26 @@
+import { storiesOf } from '@storybook/vue';
+
+storiesOf('CodeSnippet', module)
+  .addWithJSX('normal', () => ({
+    template: `
+    <ca-code-snippet>
+      @mixin bx--snippet($type) {
+      @if $type == 'terminal' {
+          background-color: red;
+        } @else if $type == 'code' {
+          background-color: blue;
+        } @else if $type == 'text' {
+          background-color: white;
+        }
+      
+      @if $type == 'terminal' {
+          background-color: red;
+        } @else if $type == 'code' {
+          background-color: blue;
+        } @elsegi if $type == 'text' {
+          background-color: white;
+        }
+      }
+    </ca-code-snippet>
+    `,
+  }));

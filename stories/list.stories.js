0a6825b0fecca7ca59186b43c10a8ad1300e3511
nicholaslee119 <nicholaslee119@gmail.com>
2018-02-04 11:44:47 +0900

    add story of list

---
+++ stories/list.stories.js
@@ -0,0 +1,18 @@
+import { storiesOf } from '@storybook/vue';
+
+storiesOf('List', module)
+  .addWithJSX('normal', () => ({
+    template: `
+    <ca-list>
+      <ca-list-item>
+        Ordered List level 1
+      </ca-list-item>
+      <ca-list-item>
+        Ordered List level 2
+      </ca-list-item>
+      <ca-list-item>
+        Ordered List level 3
+      </ca-list-item>
+    </ca-list>
+    `,
+  }));

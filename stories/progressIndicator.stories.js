9ea7869d7626f672842c12d3ac80515ad0461d87
nicholaslee119 <nicholaslee119@gmail.com>
2018-02-04 11:48:12 +0900

    add stories of progressIndicator

---
+++ stories/progressIndicator.stories.js
@@ -0,0 +1,13 @@
+import { storiesOf } from '@storybook/vue';
+
+storiesOf('ProgressIndicator', module)
+  .addWithJSX('normal', () => ({
+    template: `
+    <ca-progress-indicator>
+      <ca-progress-indicator-step isCompleted>step1</ca-progress-indicator-step>
+      <ca-progress-indicator-step isCurrent>step2</ca-progress-indicator-step>
+      <ca-progress-indicator-step>step3</ca-progress-indicator-step>
+      <ca-progress-indicator-step>step4</ca-progress-indicator-step>
+    </ca-progress-indicator>
+    `,
+  }));

ecb4c300b21a8807c04cae7ed578b1ce05d2cb3e
nicholaslee119 <nicholaslee119@gmail.com>
2018-02-07 09:49:50 +0900

    add secondary button story

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -3,6 +3,8 @@
 import { storiesOf } from '@storybook/vue';
 
 storiesOf('Button', module)
-  .addWithJSX('normal', () => ({
+  .addWithJSX('primary', () => ({
     template: '<ca-button primary>Text</ca-button>',
+  })).addWithJSX('secondary', () => ({
+    template: '<ca-button secondary>Text</ca-button>',
   }));

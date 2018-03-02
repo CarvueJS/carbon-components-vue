85e336bfbbb62a8503ebb720ec009d4a6f24639e
nicholaslee119 <nicholaslee119@gmail.com>
2018-03-02 13:54:46 +0900

    improve fileUploader

--- components/CaFileUploader/CaFileUploader.vue
+++ components/CaFileUploader/CaFileUploader.vue
@@ -4,13 +4,16 @@
       for="your-file-importer-id-here"
       class="bx--file-btn bx--btn bx--btn--secondary"
       role="button"
-      tabindex="0"><slot></slot></label>
+      tabindex="0">
+      <slot></slot>
+    </label>
     <input
       type="file"
       class="bx--file-input"
       id="your-file-importer-id-here"
       data-file-uploader
       data-target="[data-file-container]"
+      @change="change"
       multiple
     />
     <div data-file-container class="bx--file-container"></div>
@@ -18,8 +21,27 @@
 </template>
 
 <script>
+  import { FileUploader } from 'carbon-components'
+  
   export default {
     name: 'ca-file-uploader',
+    props: {
+      value: null
+    },
+    data: () => ({
+      fileUploader: {}
+    }),
+    mounted () {
+      this.fileUploader = FileUploader.create(document.querySelector('[data-file]'));
+    },
+    methods: {
+      change () {
+        // TODO: should be improved
+        var image = document.createElement('img');
+        image.src = window.URL.createObjectURL(this.fileUploader.input.files[0]);
+        this.$emit('input', image)
+      }
+    }
   };
 </script>
 
--- stories/fileUploader.stories.js
+++ stories/fileUploader.stories.js
@@ -2,7 +2,18 @@ import { storiesOf } from '@storybook/vue';
 
 storiesOf('FileUploader', module)
   .addWithJSX('normal', () => ({
+    data: () => ({
+      image: null,
+    }),
     template: `
-    <ca-file-uploader>up up</ca-file-uploader>
+    <div>
+      <ca-file-uploader v-model="image">up up</ca-file-uploader>
+      <div class="container"></div>
+    </div>
     `,
+    watch: {
+      image(newImage) {
+        document.querySelector('.container').appendChild(newImage);
+      },
+    },
   }));

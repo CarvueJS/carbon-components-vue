f5becf60b26003a722f3ae8a61e6c6a26ac17789
nicholaslee119 <nicholaslee119@gmail.com>
2018-03-06 10:10:46 +0900

    improve with model

--- components/CaToggle/CaToggle.vue
+++ components/CaToggle/CaToggle.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bx--form-item">
-    <input class="bx--toggle" id="toggle1" type="checkbox">
+    <input class="bx--toggle" id="toggle1" type="checkbox" @change="change">
     <label class="bx--toggle__label" for="toggle1">
       <span class="bx--toggle__text--left">{{this.left}}</span>
       <span class="bx--toggle__appearance"></span>
@@ -12,7 +12,13 @@
 <script>
   export default {
     name: 'ca-toggle',
+    methods: {
+      change (e) {
+        this.$emit('input', e.target.checked)
+      }
+    },
     props: {
+      value: null,
       left: {
         type: String,
         default: 'Off',
--- stories/toggle.stories.js
+++ stories/toggle.stories.js
@@ -5,4 +5,14 @@ storiesOf('Toggle', module)
     template: `
     <ca-toggle left="kai" right="guan"></ca-toggle>
     `,
+  })).addWithJSX('with model', () => ({
+    data: () => ({
+      model: true,
+    }),
+    template: `
+    <div>
+      <ca-toggle left="off" right="on" v-model="model"></ca-toggle>
+      model is {{model}}
+    </div>
+    `,
   }));

b1f510a8a6ced1a7b587c576302de65f4c3ec9bb
nicholaslee119 <nicholaslee119@gmail.com>
2018-03-06 10:03:36 +0900

    improve input with model

--- components/CaTextInput/CaTextInput.vue
+++ components/CaTextInput/CaTextInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bx--form-item">
-    <input id="text-input-1" type="text" class="bx--text-input" :placeholder="placeholder">
+    <input id="text-input-1" type="text" class="bx--text-input" :placeholder="placeholder" :value="value" @change="change">
   </div>
 </template>
 
@@ -8,11 +8,17 @@
   export default {
     name: 'ca-text-input',
     props: {
+      value: null,
       placeholder: {
         type: String,
         default: '',
       },
     },
+    methods: {
+      change (e) {
+        this.$emit('input', e.target.value)
+      }
+    }
   };
 </script>
 
--- stories/textInput.stories.js
+++ stories/textInput.stories.js
@@ -1,8 +1,18 @@
-import {storiesOf} from "@storybook/vue"
+import { storiesOf } from '@storybook/vue';
 
 storiesOf('TextInput', module)
   .addWithJSX('normal', () => ({
     template: `
     <ca-text-input placeholder="holder"></ca-text-input>
+    `,
+  })).addWithJSX('with model', () => ({
+    data: () => ({
+      model: 'text test',
+    }),
+    template: `
+    <div>
+      <ca-text-input placeholder="holder" v-model="model"></ca-text-input>
+      model is {{model}}
+    </div>
     `,
   }));

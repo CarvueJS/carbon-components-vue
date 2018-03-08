1cc3fa01e85d84a67fbe027ddfc85289684bb701
nicholaslee119 <nicholaslee119@gmail.com>
2018-03-08 11:39:30 +0900

    test of overflow menu

---
+++ components/CaOverflowMenu/CaOverflowMenu.spec.js
@@ -0,0 +1,14 @@
+import { shallow } from 'vue-test-utils';
+import CaOverflowMenu from './CaOverflowMenu.vue';
+import CaOverflowMenuOption from './CaOverflowMenuOption.vue';
+
+describe('CaOverflowMenu', () => {
+  test('is a Vue instance', () => {
+    const wrapper = shallow(CaOverflowMenu, { 
+      slots: {
+        default: CaOverflowMenuOption
+      }
+    });
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+})

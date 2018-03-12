ca3e1175871e55d9e64203eb15bb11fd146d6d1b
nicholaslee119 <nicholaslee119@gmail.com>
2018-03-12 10:05:11 +0900

    improve example

--- stories/according.stories.js
+++ stories/according.stories.js
@@ -8,8 +8,29 @@ storiesOf('According', module)
         <div slot="title">this is a title</div>
         <div slot="content"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p></div>
       </ca-accordion-item>
-      <ca-accordion-item active>
+      <ca-accordion-item>
         <div slot="title">this is a titleB</div>
+        <div slot="content"><p>this is a contentB</p></div>
+      </ca-accordion-item>
+      <ca-accordion-item active>
+        <div slot="title">this is a titleC</div>
+        <div slot="content">
+          <ca-select>
+            <ca-select-item>Choose an option</ca-select-item>
+            <ca-select-item value="solong">A much longer option that is worth having around to check how text flows</ca-select-item>
+            <ca-select-group label="Category 1">
+              <ca-select-item value="option1">11</ca-select-item>
+              <ca-select-item value="option2">22</ca-select-item>
+            </ca-select-group>
+            <ca-select-group label="Category 22">
+              <ca-select-item value="option3">33</ca-select-item>
+              <ca-select-item value="option4">44</ca-select-item>
+            </ca-select-group>
+          </ca-select>
+        </div>
+      </ca-accordion-item>
+      <ca-accordion-item>
+        <div slot="title">this is a titleD</div>
         <div slot="content"><p>this is a contentB</p></div>
       </ca-accordion-item>
     </ca-accordion>

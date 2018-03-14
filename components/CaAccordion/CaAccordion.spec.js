d398f6cb111c80076beaaa008830611b738d9216
nicholaslee119 <nicholaslee119@gmail.com>
2018-03-14 09:31:35 +0900

    fix typo

--- components/CaAccordion/CaAccordion.spec.js
+++ components/CaAccordion/CaAccordion.spec.js
@@ -2,7 +2,7 @@ import { shallow } from 'vue-test-utils';
 import CaAccordian from './CaAccordian.vue';
 import CaAccordianItem from './CaAccordianItem.vue';
 
-describe('CaAccordian', () => {
+describe('CaAccordion', () => {
   test('is a Vue instance', () => {
     const wrapper = shallow(CaAccordian, { 
       slots: {

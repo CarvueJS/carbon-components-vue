cfc11c925839fba1644a4aed6b37aac948dfb4fa
nicholaslee119 <nicholaslee119@gmail.com>
2018-03-23 11:36:39 +0900

    remove aborted files

--- components/CaModal/index.js
+++ components/CaModal/index.js
@@ -1,13 +1,7 @@
 import CaModal from './CaModal.vue';
-import CaModalContainer from './CaModalContainer.vue';
-import CaModalContent from './CaModalContent.vue';
-import CaModalHeader from './CaModalHeader.vue';
 
 CaModal.install = (Vue) => {
   Vue.component(CaModal.name, CaModal);
-  Vue.component(CaModalContainer.name, CaModalContainer);
-  Vue.component(CaModalContent.name, CaModalContent);
-  Vue.component(CaModalHeader.name, CaModalHeader);
 };
 
 export default CaModal;

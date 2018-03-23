86a8c5928e6152c3d43e1efdf130a5d840869dc2
nicholaslee119 <nicholaslee119@gmail.com>
2018-03-23 11:59:14 +0900

    refactor notification

--- components/CaNotification/CaNotification.vue
+++ components/CaNotification/CaNotification.vue
@@ -1,11 +1,12 @@
 <template>
-  <div data-notification class="bx--inline-notification bx--inline-notification--error" role="alert">
+  <div data-notification class="bx--inline-notification" :class="dynamicClass" role="alert">
     <div class="bx--inline-notification__details">
       <svg class="bx--inline-notification__icon" width="16" height="16" viewBox="0 0 16 16" fill-rule="evenodd">
         <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zM5.1 13.3L3.5 12 11 2.6l1.5 1.2-7.4 9.5z"></path>
       </svg>
       <div class="bx--inline-notification__text-wrapper">
-        <slot></slot>
+        <p class="bx--inline-notification__title">{{title}}</p>
+        <p class="bx--inline-notification__subtitle">{{subtitle}}</p>
       </div>
     </div>
     <button data-notification-btn class="bx--inline-notification__close-button" type="button">
@@ -20,6 +21,42 @@
 <script>
   export default {
     name: 'ca-notification',
+    props: {
+      error: {
+        type: Boolean,
+        default: false
+      },
+      info: {
+        type: Boolean,
+        default: false
+      },
+      success: {
+        type: Boolean,
+        default: false
+      },
+      warning: {
+        type: Boolean,
+        default: false
+      },
+      title: {
+        type: String,
+        default: ''
+      },
+      subtitle: {
+        type: String,
+        default: ''
+      }
+    },
+    computed: {
+      dynamicClass () {
+        return {
+          'bx--inline-notification--error': this.error,
+          'bx--inline-notification--info': this.info,
+          'bx--inline-notification--success': this.success,
+          'bx--inline-notification--warning': this.warning,
+        }
+      }
+    }
   };
 </script>
 
--- stories/Notification.stories.js
+++ stories/Notification.stories.js
@@ -3,9 +3,11 @@ import { storiesOf } from '@storybook/vue';
 storiesOf('Notification', module)
   .add('Default', () => ({
     template: `
-    <ca-notification>
-      <ca-notification-title>this is title</ca-notification-title>
-      <ca-notification-subtitle>this is subtitle</ca-notification-subtitle>
+    <ca-notification
+      title="this is title"
+      subtitle="this is subtitle"
+      info
+    >
     </ca-notification>
     `,
   }));

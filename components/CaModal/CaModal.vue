5969ae9f518810beae1218c25b037a3f70efc9d5
nicholaslee119 <nicholaslee119@gmail.com>
2018-03-23 11:33:12 +0900

    improve modal

--- components/CaModal/CaModal.vue
+++ components/CaModal/CaModal.vue
@@ -1,12 +1,80 @@
 <template>
-  <div data-modal id="nofooter" class="bx--modal" tabindex="-1">
-    <slot></slot>
+  <div data-modal id="modal" class="bx--modal bx--modal--danger" :class="dynamicClass" tabindex="-1">
+    <div class="bx--modal-container">
+      <div class="bx--modal-header">
+        <p class="bx--modal-header__label bx--type-delta">{{modalLabel}}</p>
+        <p class="bx--modal-header__heading bx--type-beta">{{modalHeading}}</p>
+        <button class="bx--modal-close" type="button" data-modal-close aria-label="close modal">
+          <svg class="bx--modal-close__icon" width="10" height="10" viewBox="0 0 10 10" fill-rule="evenodd">
+            <title>Close Modal</title>
+            <path d="M9.8 8.6L8.4 10 5 6.4 1.4 10 0 8.6 3.6 5 .1 1.4 1.5 0 5 3.6 8.6 0 10 1.4 6.4 5z"></path>
+          </svg>
+        </button>
+      </div>
+
+      <div class="bx--modal-content">
+        <slot></slot>
+      </div>
+
+      <div class="bx--modal-footer">
+        <button class="bx--btn bx--btn--secondary" type="button" data-modal-close>{{primaryButtonText}}</button>
+        <button class="bx--btn bx--btn--primary" type="button" data-modal-primary-focus>{{secondaryButtonText}}</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import { Modal } from 'carbon-components'
+
   export default {
     name: 'ca-modal',
+    props: {
+      modalHeading: {
+        type: String,
+        default: ''
+      },
+      modalLabel: {
+        type: String,
+        default: ''
+      },
+      primaryButtonText: {
+        type: String,
+        default: ''
+      },
+      secondaryButtonText: {
+        type: String,
+        default: ''
+      },
+      show: {
+        type: Boolean,
+        default: false
+      },
+      danger: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data: () => ({
+      modal: null
+    }),
+    watch: {
+      show(oldVal, newVal) {
+        console.log(oldVal)
+        newVal ? this.modal.show() : this.modal.hide()
+      }
+    },
+    computed: {
+      dynamicClass () {
+        return {
+          'bx--modal--danger': this.danger
+        }
+      }
+    },
+    mounted () {
+      this.modal = Modal.create(this.$el)
+      this.show ? this.modal.show() : this.modal.hide()
+    }
   };
 </script>
 
--- stories/modal.stories.js
+++ stories/modal.stories.js
@@ -3,26 +3,25 @@ import { storiesOf } from '@storybook/vue';
 storiesOf('Modal', module)
   .add('Default', () => ({
     template: `
-    <ca-tile>
-      <button class="bx--btn bx--btn--secondary" type="button" data-modal-target="#nofooter">Passive</button>
-      <ca-modal>
-        <ca-modal-container>
-          <ca-modal-header>
-            <p class="bx--modal-header__label bx--type-delta">Optional label</p>
-            <p class="bx--modal-header__heading bx--type-beta">Modal heading</p>
-            <button class="bx--modal-close" type="button" data-modal-close data-modal-primary-focus aria-label="close modal">
-              <svg class="bx--modal-close__icon" width="10" height="10" viewBox="0 0 10 10" fill-rule="evenodd">
-                <title>Close Modal</title>
-                <path d="M9.8 8.6L8.4 10 5 6.4 1.4 10 0 8.6 3.6 5 .1 1.4 1.5 0 5 3.6 8.6 0 10 1.4 6.4 5z"></path>
-              </svg>
-            </button>
-          </ca-modal-header>
-          <ca-modal-content>
-            <p>Passive modal notifications should only appear if there is an action the user needs to address immediately. Passive
-          modal notifications are persistent on the screen.</p>
-          </ca-modal-content>
-        </ca-modal-container>
+      <ca-modal
+        modalHeading="Modal heading"
+        modalLabel="Optional label"
+        primaryButtonText="Primary Button"
+        secondaryButtonText="Secondary Button"
+        show
+      >
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id accumsan augue. Phasellus consequat augue vitae tellus tincidunt posuere. Curabitur justo urna, consectetur vel elit iaculis, ultrices condimentum risus. Nulla facilisi. Etiam venenatis molestie tellus. Quisque consectetur non risus eu rutrum. </p>
       </ca-modal>
-    </ca-tile>
+    `,
+  })).add('Hide', () => ({
+    template: `
+      <ca-modal
+        modalHeading="Modal heading"
+        modalLabel="Optional label"
+        primaryButtonText="Primary Button"
+        secondaryButtonText="Secondary Button"
+      >
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id accumsan augue. Phasellus consequat augue vitae tellus tincidunt posuere. Curabitur justo urna, consectetur vel elit iaculis, ultrices condimentum risus. Nulla facilisi. Etiam venenatis molestie tellus. Quisque consectetur non risus eu rutrum. </p>
+      </ca-modal>
     `,
   }));

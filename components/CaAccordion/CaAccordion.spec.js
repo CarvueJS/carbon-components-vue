2e277335183e965bc92940123671f15a841c9bce
nicholaslee119 <nicholaslee119@gmail.com>
2018-04-03 10:44:08 +0900

    add snap tests to accordion

--- components/CaAccordion/CaAccordion.spec.js
+++ components/CaAccordion/CaAccordion.spec.js
@@ -11,4 +11,30 @@ describe('CaAccordion', () => {
     });
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
+
+  test('Mathes snapshot: pass title props', () => {
+    const wrapper = shallow(CaAccordion, { 
+      slots: {
+        default: {
+          render(h) {
+            return h(CaAccordionItem, { props: { title: 'titleText' } })
+          }
+        }
+      }
+    });
+    expect(wrapper.html()).toMatchSnapshot()
+  });
+
+  test('Mathes snapshot: pass activie props', () => {
+    const wrapper = shallow(CaAccordion, { 
+      slots: {
+        default: {
+          render(h) {
+            return h(CaAccordionItem, { props: { active: true } })
+          }
+        }
+      }
+    });
+    expect(wrapper.html()).toMatchSnapshot()
+  });
 })

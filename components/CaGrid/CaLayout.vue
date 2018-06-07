07231b1e61019b5b340fa1aef7bc6d8c18f6af58
nicholaslee119 <nicholaslee119@gmail.com>
2018-06-07 10:22:08 +0900

    imple offset and the storybook

--- components/CaGrid/CaLayout.vue
+++ components/CaGrid/CaLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="display: flex;">
+  <div class="ca-layout" :class="bindClass">
     <slot/>
   </div>
 </template>
@@ -7,9 +7,26 @@
 <script>
 export default {
   name: 'ca-layout',
+  props: {
+    wrap: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    bindClass () {
+      return this.wrap ? 'ca-layout__wrap' : ''
+    }
+  }
 };
 </script>
 
 <style>
+.ca-layout {
+  display: flex;
+}
 
+.ca-layout__wrap {
+  flex-wrap: wrap;
+}
 </style>
--- stories/grid.stories.js
+++ stories/grid.stories.js
@@ -3,13 +3,48 @@ import { storiesOf } from '@storybook/vue';
 storiesOf('Grid', module)
   .add('Default', () => ({
     template: `
-    <ca-layout>
-      <ca-flex col="x7">
-        <div>test</div>
+    <ca-layout wrap>
+      <ca-flex col="x12" >
+        <ca-tile style="background: #1867c0"/>
       </ca-flex>
-      <ca-flex col="x5">
-        test
+
+      <ca-flex v-for="i in 2" :key="i" col="x6">
+        <ca-tile style="background: #5cbbf6"/>
       </ca-flex>
+
+      <ca-flex v-for="i in 3" :key="i" col="x4">
+        <ca-tile style="background: #1867c0"/>
+      </ca-flex>
+
+      <ca-flex v-for="i in 4" :key="i" col="x3">
+        <ca-tile style="background: #5cbbf6"/>
+      </ca-flex>
+
+      <ca-flex v-for="i in 6" :key="i" col="x2">
+        <ca-tile style="background: #1867c0"/>
+      </ca-flex>
+
+    </ca-layout>
+    `,
+  })).add('Offset', () => ({
+    template: `
+    <ca-layout wrap>
+      <ca-flex col="x10" offset="x1">
+        <ca-tile style="background: #1867c0"/>
+      </ca-flex>
+
+      <ca-flex col="x6" offset="x4">
+        <ca-tile style="background: #5cbbf6"/>
+      </ca-flex>
+
+      <ca-flex col="x4" offset="x2">
+        <ca-tile style="background: #1867c0"/>
+      </ca-flex>
+
+      <ca-flex col="x3" offset="x3">
+        <ca-tile style="background: #4caf50"/>
+      </ca-flex>
+
     </ca-layout>
     `,
   }));

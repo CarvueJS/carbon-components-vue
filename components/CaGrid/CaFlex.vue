14f7a35991ada2574ef96ebc9c4e88e40200644f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-07 01:53:17 +0000

    Deploy nicholaslee119/carbon-components-vue to github.com/nicholaslee119/carbon-components-vue.git:gh-pages

--- components/CaGrid/CaFlex.vue
+++ components/CaGrid/CaFlex.vue
@@ -8,22 +8,75 @@
 export default {
   name: 'ca-flex',
   props: {
+    // TODO: the validation of props should be stronger
     col: {
       type: String,
-      default: 'x1',
+      default: null,
+    },
+    offset: {
+      type: String,
+      default: null,
     },
   },
   computed: {
     bindClass() {
-      return `ca-flex__${this.col}`;
+      return `${this.col != null ? `ca-flex__${this.col}` : ''} ${this.offset != null ? `ca-flex__offset-${this.offset}` : ''}`;
     },
   },
 };
 </script>
 
 <style scoped>
+.ca-flex__offset-x1 {
+  margin-left: 8.333333%;
+}
+
+.ca-flex__offset-x2 {
+  margin-left: 16.66667%;
+}
+
+.ca-flex__offset-x3 {
+  margin-left: 25%;
+}
+
+.ca-flex__offset-x4 {
+  margin-left: 33.333333%;
+}
+
+.ca-flex__offset-x5 {
+  margin-left: 41.66666%;
+}
+
+.ca-flex__offset-x6 {
+  margin-left: 50%;
+}
+
+.ca-flex__offset-x7 {
+  margin-left: 58.333333%;
+}
+
+.ca-flex__offset-x8 {
+  margin-left: 66.66667%;
+}
+
+.ca-flex__offset-x9 {
+  margin-left: 75%;
+}
+
+.ca-flex__offset-x10 {
+  margin-left: 83.333333%;
+}
+
+.ca-flex__offset-x11 {
+  margin-left: 91.66667%;
+}
+
+.ca-flex__offset-x12 {
+  margin-left: 100%;
+}
+
 .ca-flex__x1 {
-  flex: 0 0 8.333333%;
+  flex: 0 0 8.33333333%;
 }
 
 .ca-flex__x2 {

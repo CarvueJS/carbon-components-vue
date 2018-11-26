bb4bfcc1be7c0755d2c0005f0bb5ef42cbb69dd1
Valentin Viennot <valentin.viennot@ibm.com>
2018-11-26 12:13:54 +0000

    dropdown label

--- components/CaDropdown/CaDropdown.vue
+++ components/CaDropdown/CaDropdown.vue
@@ -1,6 +1,6 @@
 <template>
   <ul data-dropdown data-value class="bx--dropdown" tabindex="0">
-    <li class="bx--dropdown-text">Dropdown label</li>
+    <li class="bx--dropdown-text">{{label}}</li>
     <svg class="bx--dropdown__arrow" width="10" height="5" viewBox="0 0 10 5" fill-rule="evenodd">
       <path d="M10 0L5 5 0 0z"></path>
     </svg>
@@ -19,6 +19,7 @@ export default {
   name: 'ca-dropdown',
   props: {
     value: null,
+    label: 'Dropdown label',
   },
   data() {
     return {

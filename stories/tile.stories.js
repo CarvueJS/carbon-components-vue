720aef16d4af2f8759a8b3a80ced681f3aa91755
Valentin Viennot <valentin.viennot@ibm.com>
2018-11-27 16:30:51 +0000

    Clickable tile with onClick handler

--- stories/tile.stories.js
+++ stories/tile.stories.js
@@ -1,8 +1,16 @@
-import {storiesOf} from "@storybook/vue"
+import { storiesOf } from '@storybook/vue';
 
 storiesOf('Tile', module)
   .add('Default', () => ({
     template: `
     <ca-tile>tile</ca-tile>
+    `,
+  }))
+  .add('Clickable', () => ({
+    data: () => ({
+      text: 'tile',
+    }),
+    template: `
+      <ca-tile :clickHandler="() => {text = 'tile has been clicked'}">{{text}}</ca-tile>
     `,
   }));
